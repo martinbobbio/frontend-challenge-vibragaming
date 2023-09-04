989eac5bbed15afbc1bd0cb041ffc8ad3ec6e539
Martin Bobbio <martinbobbio1@gmail.com>
2023-09-04 02:55:17 -0300

    edit: update route

--- src/constants/axios/axios.ts
+++ src/constants/axios/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  // baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  baseURL:
+    import.meta.env.REACT_APP_API_URL ||
+    'https://backend-challenge-vibragaming.vercel.app/api',
 });
 
 export default api;
